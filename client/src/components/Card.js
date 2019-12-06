aa74c21cb836850919dd702ebb2bd994b499f993
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2019-12-06 12:56:40 -0800

    added test

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -8,10 +8,10 @@ const PlayersCard = (props) => {
   return (
    
       <Col xs="12" s="6" md="4">
-          <Card>
+          <Card className="card-main">
             <CardImg top width="100%" src="https://images.unsplash.com/photo-1542680283-b96aadf08a09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="women soccer" />
             <CardBody>
-              <CardTitle>Player Name: {props.name}</CardTitle>
+              <CardTitle className="card-name">{props.name}</CardTitle>
               <CardSubtitle>Country: {props.country}</CardSubtitle>
               <CardSubtitle># of searches: {props.searches}</CardSubtitle>
             </CardBody>
